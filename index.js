0dfc1d2b6add587388cd1b2207de14ddf4d9a729
zahra pourfakhar <zahrapourfakhar@zahras-MacBook-Pro.local>
2020-05-19 15:04:36 -0400

    Implemented DOM for makeing the website dynamic.

---
+++ index.js
@@ -0,0 +1,21 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomDiceImg1 = "images/dice" + randomNumber1 + ".png";
+
+let image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomDiceImg1);
+
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+let randomDiceImg2 = "images/dice" + randomNumber2 + ".png";
+
+let image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomDiceImg2);
+
+
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
